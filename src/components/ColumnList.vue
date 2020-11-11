261d0e24e040527d60b8e56012fc61f9c8ba9d35
zhangqz <2967376566@qq.com>
2020-11-11 18:03:44 +0800

    到4-6

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,13 +1,22 @@
 <template>
-  <ul>
-    <li v-for="column of list"
-        :key="column.id">
-      <img :src="column.avatar"
-           :alt="column.title">
-      <p>{{column.description}}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column of list"
+         :key="column.id"
+         class="col-4 mb-4">
+      <div class="card h-100 shadow-sm"
+           style="width: 18rem;">
+        <div class="card-body text-center">
+          <img :src="column.avatar"
+               :alt="column.title"
+               class="rounded-circle border border-light w-25 my-3">
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text text-left">{{column.description}}</p>
+          <a href="#"
+             class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
